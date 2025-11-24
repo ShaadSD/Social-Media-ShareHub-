6b6b3b9b7a570b59ea3a02133d9f187d750a48b5
Altuhim Md. Shaad <144701936+ShaadSD@users.noreply.github.com>
2025-11-24 15:12:44 +0600

    rdirect login

--- user/views.py
+++ user/views.py
@@ -60,9 +60,9 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
+        return redirect('https://social-media-sharehub.netlify.app')
     else:
-        return redirect('register')
+        return redirect('https://social-media-sharehub.netlify.app/register')
     
 
 class UserLoginApiView(APIView):
